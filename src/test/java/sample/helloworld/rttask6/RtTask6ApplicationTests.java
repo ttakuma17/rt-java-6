acf2721b770a9847a23e1d930afc52774f3eeedf
ttakuma17 <ttanimiya@gmail.com>
2022-12-27 21:35:21 +0900

    Hello Worldを表示

---
+++ src/test/java/sample/helloworld/rttask6/RtTask6ApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.helloworld.rttask6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtTask6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
